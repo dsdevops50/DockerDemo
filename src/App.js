c17ca4bb34d1f282321d64d8523ed00b4b5366f2
Dharmendra Singh <88479174+dsdevops50@users.noreply.github.com>
2023-07-09 14:26:36 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.Welcome to my test site.
         </p>
-        <h1 class="title">Hello All This is my Testing site</h1>
+        <h1 class="title">Hello All This is my Testing site (Branch:Devtest)</h1>
         <p id="currentTime"></p>
         <script src="script.js"></script>
         <a
